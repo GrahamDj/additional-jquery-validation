a11d5dccfbc52d17dd70fc44a1ffee8d7eedd813
Martin Langenberg <langenbergmartin@gmail.com>
2018-09-23 23:30:44 +0200

    Added inlist and alphanumericspaces methods

---
+++ src/additional-methods-ci.js
@@ -0,0 +1,15 @@
+/**
+ * Additional jQuery Form Validation methods that match the rules used in CodeIgniter Form Validation
+ */
+
+$.validator.addMethod( "inlist", function( value, element, param ) {
+    if ( this.optional( element ) ) {
+        return true;
+    }
+
+    return value.indexOf(param) >= 0;
+}, $.validator.format( "{0} is not allowed. Please enter a set value" ) );
+
+$.validator.addMethod( "alphanumericspaces", function( value, element ) {
+    return this.optional( element ) || /^[\w\s]+$/i.test( value.trim() );
+}, "Letters, numbers, underscores and spaces only please" );
